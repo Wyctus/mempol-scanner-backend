2e98da9e3d48ec5e21a637cc9964c5aea1c1b913
Wyctus <ujszaszi.zzoltan@gmail.com>
2022-08-14 21:29:44 +0200

    feat: implemented SimpleMempoolProvider

--- src/provider/SimpleMempoolProvider.ts
+++ src/provider/SimpleMempoolProvider.ts
@@ -2,19 +2,15 @@ import chalk from "chalk";
 import { providers } from "ethers";
 import MempoolProvider from "./MempoolProvider";
 import { isEIP1559Transaction, isLegacyTransaction, LegacyMempoolTransaction, MempoolTransaction } from "./MempoolTransaction";
-
 /**
  * The `SimpleMempoolProvider` is an implementation of the `MempoolProvider`.
  * It is based on an Ethereum node websocket provider.
  */
 class SimpleMempoolProvider extends MempoolProvider {
   provider: providers.InfuraWebSocketProvider;
-
   constructor(capacity?: number) {
     super(capacity);
-
     this.provider = new providers.InfuraWebSocketProvider("mainnet", "bc4844cc889e45be9722e5b565eaefdd");
-
     this.provider.on("pending", (tx) => {
       this.provider.getTransaction(tx).then((transaction) => {
         if (transaction) {
@@ -29,7 +25,6 @@ class SimpleMempoolProvider extends MempoolProvider {
       console.log(chalk.bold.red(`Unknown transaction type: ${tx.type}, transaction is ignored.`));
       return;
     }
-
     const transObj: Partial<MempoolTransaction> = {
       type: tx.type,
       hash: tx.hash,
@@ -40,16 +35,13 @@ class SimpleMempoolProvider extends MempoolProvider {
       data: Buffer.from(tx.data),
       gasLimit: tx.gasLimit.toNumber(),
     };
-
     if (isLegacyTransaction(transObj)) {
       transObj.gasPrice = tx.gasPrice?.toNumber() as number;
     } else if (isEIP1559Transaction(transObj)) {
       transObj.maxPriorityFeePerGas = tx.maxPriorityFeePerGas?.toNumber() as number;
       transObj.maxFeePerGas = tx.maxFeePerGas?.toNumber() as number;
     }
-
     this.notify(transObj as MempoolTransaction);
   }
 }
-
 export default SimpleMempoolProvider;
